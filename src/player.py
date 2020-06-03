25ccf8ddb0c8fd3f33ae577b1bcd8e1a0c7ba647
JWilson1172 <jw59615@gmail.com>
2020-06-03 14:46:02 -0400

    progress on game assignment

--- src/player.py
+++ src/player.py
@@ -1,2 +1,14 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+from room import Room
+
+
+class Player:
+    def __init__(self, room: Room, name: str):
+        self.room = room
+        self.name = name
+        return
+
+    def move(self, direction: str):
+        print("NOT IMPLEMENTED")
+        raise NotImplementedError("please wait unitl the feature is implemented")
